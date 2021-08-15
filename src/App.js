1506baac03b09546955e759f59f0cd6fe6abee24
Vladyslav <vladtravel2015@gmail.com>
2021-08-15 17:08:48 +0300

    finished home work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import ContactList from "./components/ContactsList/ContactsList";
+import ContactForm from "./components/ContactForm/ContactForm";
+import Filter from "./components/Filter/Filter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <p className="App-find">Find contacts by name</p>
+      <Filter />
+      <ContactList />
     </div>
   );
 }
